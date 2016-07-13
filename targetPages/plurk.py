235c28cfadf1310e8df3533df5a5c93081bc83a4
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2016-07-14 04:11:22 +0800

    Now support plurk

---
+++ targetPages/plurk.py
@@ -0,0 +1,21 @@
+import requests
+
+from interfaces import ITargetPage
+
+class Target_page(ITargetPage):
+    def __init__(self):
+        super().__init__('Plurk', email_as_account=False)
+
+    def check_result(self, possible_account_list):
+        result = []
+
+        for account in possible_account_list:
+            url = 'https://github.com/%s' % account
+            r = requests.get(url)
+
+            if r.content.decode('utf-8').find('User Not Found') >= 0:
+                result.append((account, False))
+            else:
+                result.append((account, True))
+
+        return result
